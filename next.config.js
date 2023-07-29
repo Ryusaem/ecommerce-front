de2589f78702dc47adf4c0c0a679fc5a0ea2e984
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2023-07-29 18:17:26 +0200

    Styling Header+Nav Bar with styled-components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // compiler styledComponents is used to auto import styled-components
+  compiler: {
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
 import Header from "@/components/Header";
-import { styled } from "styled-components";
-
-const StyledHeader = styled.header`
-  background-color: #222;
-`;
 
 export default function HomePage() {
-  return (
-    <StyledHeader>
-      <Header />
-    </StyledHeader>
-  );
+  return <Header />;
 }
