b4f50a30d829bf93c4e23b45e382ca8573334e95
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2023-08-21 17:45:08 +0200

    feat: cart structure

--- pages/cart.js
+++ pages/cart.js
@@ -1,9 +1,30 @@
+import Center from "@/components/Center";
 import Header from "@/components/Header";
+import styled from "styled-components";
+
+const ColumnWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1.3fr 0.7fr;
+  gap: 40px;
+  margin-top: 40px;
+`;
+
+const Box = styled.div`
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 30px;
+`;
 
 export default function CartPage() {
   return (
     <>
       <Header />
+      <Center>
+        <ColumnWrapper>
+          <Box>1</Box>
+          <Box>2</Box>
+        </ColumnWrapper>
+      </Center>
     </>
   );
 }
