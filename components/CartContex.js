1044a8202e71d2c67b9eb4341e24158208117117
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2023-08-21 15:41:19 +0200

    feat: cart counter local + server

--- components/CartContex.js
+++ components/CartContex.js
@@ -4,16 +4,25 @@ const { createContext, useState, useEffect } = require("react");
 export const CartContext = createContext({});
 
 export function CartContexProvider({ children }) {
-  // we can use this state to store the products in the cart
-  const [cartProducts, setCartProducts] = useState([
-    JSON.parse(localStorage.getItem("cart")) || [],
-  ]);
+  // we need to check if we are in the browser or not and if we are we can use the local storage
+  const ls = typeof window !== "undefined" ? window.localStorage : null;
 
+  // we can use this state to store the products in the cart
+  const [cartProducts, setCartProducts] = useState([]);
+
+  // we need to check if the cartProducts state has changed and if it has we need to update the local storage. It will refresh the local storage every time the cartProducts state changes
   useEffect(() => {
     if (cartProducts?.length > 0) {
-      localStorage.setItem("cart", JSON.stringify(cartProducts));
+      ls?.setItem("cart", JSON.stringify(cartProducts));
     }
   }, [cartProducts]);
+
+  // we check if the local storage has a cart and if it does we set the cartProducts state to the local storage cart
+  useEffect(() => {
+    if (ls && ls.getItem("cart")) {
+      setCartProducts(JSON.parse(ls.getItem("cart")));
+    }
+  }, []);
 
   // we can use this function to add a product to the cart
   function addProduct(productId) {
